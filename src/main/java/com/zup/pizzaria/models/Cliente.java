f511b10c98b7fa2007c2823f770c070317df02a3
RodolffoNMS <rodolffo.souza@zup.com.br>
2025-01-20 16:05:33 -0300

    feat: Classe Model comentada para consulta e estudos.

--- src/main/java/com/zup/pizzaria/models/Cliente.java
+++ src/main/java/com/zup/pizzaria/models/Cliente.java
@@ -1,24 +1,48 @@
 package com.zup.pizzaria.models;
+/*
+* Explicação dos principais pontos:
+* Anotações JPA:
+* @Entity: Indica que a classe será mapeada para uma tabela no banco de dados.
+* @Id: Define o campo como chave primária.
+* @GeneratedValue: Configura a estratégia de geração automática do ID.
+* @Column: Configura as propriedades da coluna no banco de dados, como nullable e unique.
+* Construtor padrão:
+* Necessário para o Hibernate criar instâncias da entidade.
+* Construtor com parâmetros:
+* Facilita a criação de objetos Cliente com os valores necessários.
+* Getters e Setters:
+* Permitem acessar e modificar os valores dos campos da entidade.
+*/
 
 import jakarta.persistence.*;
 
+// Indica que esta classe é uma entidade JPA que será mapeada para uma tabela no banco de dados.
 @Entity
 
 public class Cliente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id// Define o campo 'id' como chave primária da tabela.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// Gera automaticamente o valor do ID, utilizando a estratégia de auto incremento do banco de dados.
     private Long id;
-    @Column(nullable = false)//Garante que o valor não seja nulo no banco de dados.
+
+    //Garante que o valor não seja nulo no banco de dados.
+    @Column(nullable = false)
     private String nome;
-    @Column(nullable = false, unique = true)// Além de não nulo, agora ele deve ser único.
+
+    // Além de não nulo, agora ele deve ser único.
+    @Column(nullable = false, unique = true)
     private String email;
+
+    //Garante que o valor não seja nulo no banco de dados.
     @Column(nullable = false)
     private String telefone;
 
-    // Construtor padrão (obrigatório para o Hibernate)
+    // Construtor padrão (obrigatório para o Hibernate).
+    // O Hibernate exige um construtor sem argumentos para criar instâncias da entidade.
     public Cliente() {
     }
-
+    // Construtor para inicializar os campos da entidade.
+    // Este construtor é usado para criar uma instância de Cliente com os valores fornecidos.
     public Cliente(String nome, String email, String telefone) {
         this.nome = nome;
         this.email = email;
