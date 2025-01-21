50d7245f1b94ff4c44a0cc1e47ec44aeae9f101c
RodolffoNMS <rodolffo.souza@zup.com.br>
2025-01-21 14:37:10 -0300

    feat: > Cria PizzaRepository para interagir com o banco

---
+++ src/main/java/com/zup/pizzaria/repository/PizzaRepository.java
@@ -0,0 +1,9 @@
+package com.zup.pizzaria.repository;
+
+import com.zup.pizzaria.models.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PizzaRepository extends JpaRepository<Pizza, Long> {
+}
